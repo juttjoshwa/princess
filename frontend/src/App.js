2e13b46e2fe3bf22810a2a1c8a37fff59b78b415
prxncxss03 <pocon.evangeline@gmail.com>
2022-07-13 14:32:44 +0800

    add style.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
+/* hooks */
 import { useEffect, useState } from "react";
+
+/* library */
 import axios from "axios";
+
+/* styles */
+import "./style.css";
 
 function App() {
   const [name,setName]= useState('')
   const [email,setEmail]= useState('')
   const [users,setUsers]= useState ([])
 
+  /* fetch all the data from the database */
   useEffect(() => {
     axios.get("http://localhost:4050/users").then(response => {
       console.log(response.data);
@@ -15,14 +22,17 @@ function App() {
     })
   },[])
 
+  /* Add User to the database */
   const handleSubmit = (e) => {
    e.preventDefault()
    console.log("inside handle submit")
    axios.post(`http://localhost:4050/users`,{
-     name: name,
-     Email : email
+     name: name || "Empty",
+     Email : email || "Empty"
   }).then((response) => {
     console.log(response.data.data.name);
+   
+    /* automatic update from the browser after adding to DB*/
     setUsers([...users,response.data.data])
   }).catch(error => {
     console.log(`error from server ${error}`)
@@ -38,23 +48,21 @@ function App() {
   
 
   return (
-    <div>
+    <div className="Container">
       <form action="/" method="post" >
         <label>Name</label>
-        <input type= 'text' value={name} onChange={e => setName(e.target.value)} />
+        <input type= 'text' value={name} onChange={e => setName(e.target.value)} required />
         <br/>
         <label>Email</label>
-        <input type='text'  value={email} onChange={e => setEmail(e.target.value)}/>
-        <button type="Submit" onClick={(e) => handleSubmit(e)}>Submit</button>
-
-        <div >
+        <input type='email'  value={email} onChange={e => setEmail(e.target.value)} required/>
+        <button className="submit-btn" type="Submit" onClick={(e) => handleSubmit(e)}>Submit</button>
+      </form>
+      <div  className="card-container">
           {users.map(user=>
-            <div key={user._id}>
+            <div key={user._id} className="card">
             <p>{user.name}</p>
           </div>)}
         </div>
-
-      </form>
 
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 import mongoose from "mongoose";
 import User  from "./User.js";
+/*  */
 import dotenv from "dotenv";
 dotenv.config();
 
